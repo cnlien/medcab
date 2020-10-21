c283640642682f583c560aa8c9265e121bfb72cd
Chris Lien <clien@sharpencx.com>
2020-10-21 11:55:02 -0400

    changed the way state is collected on strain info page

--- src/components/StrainInformation.js
+++ src/components/StrainInformation.js
@@ -15,72 +15,65 @@ import strainPlaceholder from '../images/strain_placeholder.jpg'
 
 const StrainInformation = (props) => {
 
+    const [data, setData] = useState({
+        fetching: true,
+        description: null,
+        effects: null,
+        flavors: null,
+        indica: null,
+        sativa: null,
+        hybrid: null
+    })
+
+    console.log("data from state", data)
+
+    const [fetching, setFetching] = useState(false)
     const [description, setDescription] = useState({})
-    const [effects, setEffects] = useState([])
-    const [flavors, setFlavors] = useState([])
-    const [indica, setIndica] = useState([])
-    const [sativa, setSativa] = useState([])
-    const [hybrid, setHybrid] = useState([])
+    const [effects, setEffects] = useState(["Loading..."])
+    const [flavors, setFlavors] = useState(["Loading..."])
+    const [indica, setIndica] = useState(["Loading..."])
+    const [sativa, setSativa] = useState(["Loading..."])
+    const [hybrid, setHybrid] = useState(["Loading..."])
+
+    console.log("Medical Effects",effects.medical)
+    console.log(fetching)
 
     useEffect(() => {
-        const id = props.match.params.id
-        axios
-            .get(`https://med-cab-bw.herokuapp.com/api/strains/${id}/effects`)
-            .then((res) => {
-                setEffects(res.data)
-            })
-            .catch((err)=> {
-                console.log(err)
-            })
+        const fetchData = async () => {
+            const id = props.match.params.id
+            const description = await axios(
+                `https://med-cab-bw.herokuapp.com/api/strains/${id}/desc`
+            );
+            const effects = await axios(
+                `https://med-cab-bw.herokuapp.com/api/strains/${id}/effects`
+            );
+            const flavors = await axios(
+                `https://med-cab-bw.herokuapp.com/api/strains/${id}/flavors`
+            );
+            const indica = await axios(
+                `https://med-cab-bw.herokuapp.com/api/strains/indica/`
+            );
+            const sativa = await axios(
+                `https://med-cab-bw.herokuapp.com/api/strains/sativa/`
+            );
+            const hybrid = await axios(
+                `https://med-cab-bw.herokuapp.com/api/strains/hybrid/`
+            );
 
-        axios
-            .get(`https://med-cab-bw.herokuapp.com/api/strains/${id}/flavors`)
-            .then((res) => {
-                setFlavors(res.data)
+            setData({
+                fetching: false,
+                description: description.data,
+                effects: effects.data,
+                flavors: flavors.data,
+                indica: indica.data,
+                sativa: sativa.data,
+                hybrid: hybrid.data
             })
-            .catch((err)=> {
-                console.log(err)
-            })
-        axios
-            .get(`https://med-cab-bw.herokuapp.com/api/strains/${id}/desc`)
-            .then((res) => {
-                setDescription(res.data)
-            })
-            .catch((err)=> {
-                console.log(err)
-            })
-        axios
-            .get(`https://med-cab-bw.herokuapp.com/api/strains/indica/`)
-            .then((res) => {
-                setIndica(res.data)
-            })
-            .catch((err)=> {
-                console.log(err)
-            })
-
-        axios
-            .get(`https://med-cab-bw.herokuapp.com/api/strains/sativa/`)
-            .then((res) => {
-                setSativa(res.data)
-            })
-            .catch((err)=> {
-                console.log(err)
-            })
-        axios
-            .get(`https://med-cab-bw.herokuapp.com/api/strains/hybrid/`)
-            .then((res) => {
-                setHybrid(res.data)
-            })
-            .catch((err)=> {
-                console.log(err)
-            })
-            
-        
+        }
+        fetchData();
     }, []);
 
     const strainId = parseInt(props.match.params.id)
-    // console.log("Strain ID", parseInt(strainId))
-
     const isHybrid = hybrid.filter(hybrid => hybrid.id === strainId)
     const isSativa = sativa.filter(sativa => sativa.id === strainId)
     const isIndica = indica.filter(indica => indica.id === strainId)
@@ -99,6 +92,9 @@ const StrainInformation = (props) => {
                 </h1>
                 <h3>Common Effects</h3>
                 <h4>Positive Effects</h4>
+                {/* {effects.medical.map(medicalEffect => {
+                    return(medicalEffect)
+                })} */}
                 <p>{effects.positive}</p>
                 <h3>Details</h3>
                 <p>{description.desc}</p>
