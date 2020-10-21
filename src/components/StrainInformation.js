878aae03dc6fac13bb59d26d49ff2cd8041117b8
Chris Lien <clien@sharpencx.com>
2020-10-21 13:43:56 -0400

    Styled strain information page top section

--- src/components/StrainInformation.js
+++ src/components/StrainInformation.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-// REDUX
-import { connect } from 'react-redux'
-
 //COMPONENTS
 import { 
     Col,
-    Container, Row,
+    Collapse,
+    Container,
+    Row,
+    Spinner
 } from 'reactstrap'
+
+// STYLES
+import '../styles/strainInformation.scss';
 
 // IMAGES
 import strainPlaceholder from '../images/strain_placeholder.jpg'
+import ChevronDown from '../images/icons/ChevronDown.svg'
+import ChevronUp from '../images/icons/ChevronUp.svg'
 
 const StrainInformation = (props) => {
 
@@ -22,21 +27,13 @@ const StrainInformation = (props) => {
         flavors: null,
         indica: null,
         sativa: null,
-        hybrid: null
+        hybrid: null,
     })
-
-    console.log("data from state", data)
-
-    const [fetching, setFetching] = useState(false)
-    const [description, setDescription] = useState({})
-    const [effects, setEffects] = useState(["Loading..."])
-    const [flavors, setFlavors] = useState(["Loading..."])
-    const [indica, setIndica] = useState(["Loading..."])
-    const [sativa, setSativa] = useState(["Loading..."])
-    const [hybrid, setHybrid] = useState(["Loading..."])
-
-    console.log("Medical Effects",effects.medical)
-    console.log(fetching)
+    const [effectIsOpen, setEffectIsOpen] = useState(false)
+    const [flavorIsOpen, setFlavorIsOpen] = useState(false)
+    const toggleEffects = () => setEffectIsOpen(!effectIsOpen)
+    const toggleFlavors = () => setFlavorIsOpen(!flavorIsOpen)
+    
 
     useEffect(() => {
         const fetchData = async () => {
@@ -74,34 +71,95 @@ const StrainInformation = (props) => {
     }, []);
 
     const strainId = parseInt(props.match.params.id)
-    const isHybrid = hybrid.filter(hybrid => hybrid.id === strainId)
-    const isSativa = sativa.filter(sativa => sativa.id === strainId)
-    const isIndica = indica.filter(indica => indica.id === strainId)
+    
+    const StrainName = () => {
 
-    return(
-        <Container>
-            <Row>
-                <Col>
-                    <img src={strainPlaceholder} alt={`strain placeholder ${strainId}`} />
-                </Col>
-                <Col>
-                <h1>
-                    {isIndica.map((sativa) => {return(sativa.name)})}
+        if(!data.fetching) {
+            const isHybrid = data.hybrid.filter(hybrid => hybrid.id === strainId)
+            const isSativa = data.sativa.filter(sativa => sativa.id === strainId)
+            const isIndica = data.indica.filter(indica => indica.id === strainId)
+            return(
+                <h1 className="strain-name">
+                    {isIndica.map((indica) => {return(indica.name)})}
                     {isSativa.map((sativa) => {return(sativa.name)})}
                     {isHybrid.map((hybrid) => {return(hybrid.name)})}
                 </h1>
-                <h3>Common Effects</h3>
-                <h4>Positive Effects</h4>
-                {/* {effects.medical.map(medicalEffect => {
-                    return(medicalEffect)
-                })} */}
-                <p>{effects.positive}</p>
-                <h3>Details</h3>
-                <p>{description.desc}</p>
-                </Col>
-            </Row>
+            )
+        }
+    }
+
+    return(
+        <Container fluid className="strain-information-container">
+            {data.fetching ? (
+                <Spinner />
+            ) : (
+                <Row>
+                    <Col md="6" className="strain-image">
+                        <img src={strainPlaceholder} alt={`strain placeholder ${strainId}`} style={{ width: '100%' }} />
+                    </Col>
+                    <Col md="6" className="strain-information">
+                        <StrainName />
+                        {/* STRAIN DESCRIPTION */}
+                        <h3 className="strain-information-title">Details</h3>
+                        <p className="strain-information-description">{data.description.desc}</p>
+                        
+                        {/* STRAIN EFFECTS */}
+                        {effectIsOpen ? (
+                            <h3 className="strain-information-title" onClick={toggleEffects}>Common Effects <img src={ChevronUp} /></h3>
+                        ) : (
+                            <h3 className="strain-information-title" onClick={toggleEffects}>Common Effects <img src={ChevronDown} /></h3>
+                        )}
+                        <Collapse isOpen={effectIsOpen}>
+                            <Row className="strain-effects">
+                                <Col className="positive-effects">
+                                    <h6 className="strain-effects-title">Positive Effects</h6>
+                                    {data.effects.positive.map(medicalEffect => {
+                                        return(
+                                            <p className="strain-effect">{medicalEffect}</p>
+                                        )
+                                    })}
+                                </Col>
+                                <Col className="negative-effects">
+                                    <h6 className="strain-effects-title">Negative Effects</h6>
+                                    {data.effects.negative.map(medicalEffect => {
+                                        return(
+                                            <p className="strain-effect">{medicalEffect}</p>
+                                        )
+                                    })}
+                                </Col>
+                                <Col className="medical-effects">
+                                    <h6 className="strain-effects-title">Medical Effects</h6>
+                                    {data.effects.medical.map(medicalEffect => {
+                                        return(
+                                            <p className="strain-effect">{medicalEffect}</p>
+                                        )
+                                    })}
+                                </Col>
+                            </Row>
+                        </Collapse>
+                        
+                        {/* STRAIN FLAVORS */}
+                        {flavorIsOpen ? (
+                            <h3 className="strain-information-title" onClick={toggleFlavors}>Tastes &amp; Flavors <img src={ChevronUp} /></h3>
+                        ) : (
+                            <h3 className="strain-information-title" onClick={toggleFlavors}>Tastes &amp; Flavors <img src={ChevronDown} /></h3>
+                        )}
+                        <Collapse isOpen={flavorIsOpen}>
+                            <Row className="strain-flavors">
+                                <Col>
+                                    {data.flavors.map(flavor => {
+                                        return(
+                                            <p className="strain-flavor">{flavor}</p>
+                                        )
+                                    })}
+                                </Col>
+                            </Row>
+                        </Collapse>
+                    </Col>
+                </Row>
+            )}
         </Container>
     )
 }
 
-export default connect()(StrainInformation)
+export default StrainInformation
